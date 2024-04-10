2a57bd19120d8ecb729569a6130f544f4a3845ed
oguzhanaydiin <oguuzaydinn@gmail.com>
2024-04-10 23:59:59 +0200

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  // Set the base URL to match the GitHub repository name for GitHub Pages deployment
+  base: '/roadsurfer-vue/'
 })
